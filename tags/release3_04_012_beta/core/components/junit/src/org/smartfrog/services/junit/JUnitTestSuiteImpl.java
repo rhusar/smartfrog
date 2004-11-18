3419c52b8c87217c71b5e6634f79648a0620ca72
nobody <nobody@9868f95a-be1e-0410-b3e3-a02e98b909e6>
2004-11-18 13:08:06 +0000

    This commit was manufactured by cvs2svn to create tag 'release3_04_012_beta'.

---
+++ tags/release3_04_012_beta/core/components/junit/src/org/smartfrog/services/junit/JUnitTestSuiteImpl.java
@@ -0,0 +1,375 @@
+/** (C) Copyright 1998-2004 Hewlett-Packard Development Company, LP
+
+ This library is free software; you can redistribute it and/or
+ modify it under the terms of the GNU Lesser General Public
+ License as published by the Free Software Foundation; either
+ version 2.1 of the License, or (at your option) any later version.
+
+ This library is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ Lesser General Public License for more details.
+
+ You should have received a copy of the GNU Lesser General Public
+ License along with this library; if not, write to the Free Software
+ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+ For more information: www.smartfrog.org
+
+ */
+package org.smartfrog.services.junit;
+
+import junit.framework.AssertionFailedError;
+import junit.framework.Test;
+import junit.framework.TestResult;
+import org.smartfrog.sfcore.common.SmartFrogException;
+import org.smartfrog.sfcore.common.SmartFrogInitException;
+import org.smartfrog.sfcore.common.SmartFrogRuntimeException;
+import org.smartfrog.sfcore.logging.Log;
+import org.smartfrog.sfcore.prim.PrimImpl;
+import org.smartfrog.sfcore.security.SFClassLoader;
+import org.smartfrog.sfcore.utils.ComponentHelper;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.rmi.RemoteException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.regex.Pattern;
+
+
+
+/**
+ * Implementation of the Junit test suite component.
+ * This is where tests are actually run; we bring up Junit internally and run it here
+ * created 14-May-2004 15:14:23
+ */
+
+public class JUnitTestSuiteImpl extends PrimImpl implements JUnitTestSuite, junit.framework.TestListener {
+
+    private Log log;
+
+    private ComponentHelper helper;
+
+    private List classes;
+
+    private boolean ifValue;
+
+    private boolean unlessValue;
+
+
+    private String packageValue;
+
+    private RunnerConfiguration configuration;
+
+    private Statistics stats;
+
+
+
+    /**
+     * test class list we build up
+     */
+    private HashMap testClasses;
+
+    public JUnitTestSuiteImpl() throws RemoteException {
+        helper = new ComponentHelper(this);
+    }
+
+
+
+    public boolean getIf() throws RemoteException {
+        return ifValue;
+    }
+
+    public void setIf(boolean ifValue) throws RemoteException {
+        this.ifValue = ifValue;
+    }
+
+    public boolean getUnless() throws RemoteException {
+        return unlessValue;
+    }
+
+    public void setUnless(boolean unlessValue) throws RemoteException {
+        this.unlessValue = unlessValue;
+    }
+
+    /**
+     * bind to the configuration. A null parameter means 'stop binding'
+     *
+     * @param configuration
+     * @throws java.rmi.RemoteException
+     */
+    public void bind(RunnerConfiguration configuration) throws RemoteException {
+        this.configuration = configuration;
+    }
+
+    /**
+     * Called after instantiation for deployment purposed. Heart monitor is
+     * started and if there is a parent the deployed component is added to the
+     * heartbeat. Subclasses can override to provide additional deployment
+     * behavior.
+     *
+     * @throws org.smartfrog.sfcore.common.SmartFrogException
+     *                                  error while deploying
+     * @throws java.rmi.RemoteException In case of network/rmi error
+     */
+    public synchronized void sfDeploy() throws SmartFrogException, RemoteException {
+        super.sfDeploy();
+        log = helper.getLogger();
+    }
+
+    /**
+     * Can be called to start components. Subclasses should override to provide
+     * functionality Do not block in this call, but spawn off any main loops!
+     *
+     * @throws org.smartfrog.sfcore.common.SmartFrogException
+     *                                  failure while starting
+     * @throws java.rmi.RemoteException In case of network/rmi error
+     */
+    public synchronized void sfStart() throws SmartFrogException, RemoteException {
+        super.sfStart();
+        readConfiguration();
+        //runTests();
+    }
+
+    protected void readConfiguration() throws SmartFrogException, RemoteException {
+        ifValue=sfResolve(ATTR_IF,ifValue,false);
+        unlessValue=sfResolve(ATTR_UNLESS,unlessValue,false);
+        classes = flattenStringList((List)sfResolve(ATTR_CLASSES,classes,false),ATTR_CLASSES);
+
+        //package attribute names a package
+        packageValue = sfResolve(ATTR_PACKAGE,packageValue,false);
+        if(packageValue==null) {
+            packageValue="";
+        } else {
+            //add a dot at the end if we need it
+            if(packageValue.length()>0 && packageValue.charAt(packageValue.length()-1)!='.') {
+                packageValue+='.';
+            }
+        }
+        buildClassList();
+    }
+
+    /**
+     * build the list of classes to run
+     */
+    protected void buildClassList() {
+        testClasses = new HashMap();
+        Iterator it=classes.iterator();
+        while (it.hasNext()) {
+            String testclass = (String) it.next();
+            addTest(testclass);
+        }
+    }
+
+    /**
+     * add a test to the list , prepending the package
+     * @param classname
+     */
+    private void addTest(String classname) {
+        String fullname=packageValue+classname;
+
+        if(testClasses.get(fullname)==null) {
+            log.debug("adding test "+ fullname);
+            testClasses.put(fullname, fullname);
+        }
+    }
+
+    /**
+     * flatten a string list, validating type as we go. recurses as much as we need to.
+     * At its most efficient if no flattening is needed.
+     * @param src
+     * @param name
+     * @return
+     * @throws SmartFrogInitException
+     */
+    private List flattenStringList(final List src,String name) throws SmartFrogInitException {
+        List dest=new ArrayList(src.size());
+        Iterator index = src.iterator();
+        while (index.hasNext()) {
+            Object element = (Object) index.next();
+            if (element instanceof List) {
+                dest.addAll(flattenStringList((List)element,name));
+            } else if (!(element instanceof String)) {
+                throw new SmartFrogInitException("An element in "
+                        + name + " is not string: " + element.toString());
+            }
+            dest.add(element);
+        }
+        return dest;
+    }
+
+    /**
+     * run the test
+     * bail out if we were interrupted.
+     * @return true iff all tests passed
+     * @throws java.rmi.RemoteException
+     */
+    public boolean runTests() throws RemoteException, SmartFrogException {
+        if(configuration==null || getListener()==null) {
+            throw new SmartFrogException("TestSuite has not been configured yet");
+        }
+
+        if(!getIf() || getUnless()) {
+            log.debug("Skipping test as conditions preclude it");
+            return true;
+        }
+
+        boolean failed=false;
+        Iterator it= testClasses.keySet().iterator();
+        while (it.hasNext()) {
+            String classname = (String) it.next();
+            try {
+                testSingleClass(classname);
+                updateResultAttributes(false);
+            } catch (ClassNotFoundException e) {
+                throw SmartFrogException.forward(e);
+            } catch (IllegalAccessException e) {
+                throw SmartFrogException.forward(e);
+            } catch (InvocationTargetException e) {
+                throw SmartFrogException.forward(e);
+            }
+            if(Thread.currentThread().isInterrupted()) {
+                log.debug("Interrupted test thread");
+                return false;
+            }
+            updateResultAttributes(true);
+            failed = !stats.isSuccessful();
+            if(failed && !configuration.getKeepGoing()) {
+                return false;
+            }
+        }
+        return !failed;
+    }
+
+    /**
+     * write all our state to the results
+     * order it so that we set the finished last
+     */
+    private void updateResultAttributes(boolean finished) throws SmartFrogRuntimeException, RemoteException {
+        stats.updateResultAttributes(this,finished);
+    }
+
+    /**
+     * test a single class
+     * @param classname
+     * @throws ClassNotFoundException
+     * @throws IllegalAccessException
+     * @throws InvocationTargetException
+     */
+    private void testSingleClass(String classname) throws ClassNotFoundException, IllegalAccessException,
+            InvocationTargetException {
+        log.debug("testing "+classname);
+        Test tests;
+        Class clazz=loadTestClass(classname);
+        tests=extractTest(clazz);
+        TestResult result=new TestResult();
+        result.addListener(this);
+        tests.run(result);
+    }
+
+
+    /**
+     * load the test class, using the secure classloader framework.
+     *
+     * @param classname
+     * @return
+     * @throws ClassNotFoundException
+     */
+    private Class loadTestClass(String classname) throws ClassNotFoundException {
+        //Class.forName(classname);
+        Class clazz=SFClassLoader.forName(classname);
+        return clazz;
+    }
+
+    /**
+     * get the tests from the class, either as a suite or as introspected tests
+     * @param clazz
+     * @return the test
+     * @throws IllegalAccessException
+     * @throws InvocationTargetException
+     */
+    private Test extractTest(Class clazz) throws IllegalAccessException, InvocationTargetException {
+        //todo: verify that the class implements test or testsuite
+        try {
+            // check if there is a suite method
+            Method method = clazz.getMethod("suite", new Class[0]);
+            return (Test) method.invoke(null, new Class[0]);
+        } catch (NoSuchMethodException e) {
+            //if not, assume that it is a testclass and do it that way
+            return new junit.framework.TestSuite(clazz);
+        }
+    }
+
+    /**
+     * get our listener
+     * @return
+     */
+    public TestListener getListener() {
+        return configuration.getListener();
+    }
+
+    /**
+     * An error occurred.
+     */
+    public void addError(Test test, Throwable throwable) {
+        stats.incErrors();
+        TestInfo info=new TestInfo(test,throwable);
+        try {
+            getListener().addError(info);
+        } catch (RemoteException e) {
+            IgnoreRemoteFault(e);
+        }
+    }
+
+    /**
+     * ignore a remote fault by logging it
+     * @param e
+     */
+    private void IgnoreRemoteFault(RemoteException e) {
+        log.warn("ignoring",e);
+    }
+
+    /**
+     * A failure occurred.
+     */
+    public void addFailure(Test test, AssertionFailedError error) {
+        stats.incFailures();
+        TestInfo info = new TestInfo(test, error);
+        try {
+            getListener().addFailure(info);
+        } catch (RemoteException e) {
+            IgnoreRemoteFault(e);
+        }
+    }
+
+    /**
+     * A test ended.
+     */
+    public void endTest(Test test) {
+        stats.incTestsRun();
+        TestInfo info = new TestInfo(test);
+        try {
+            getListener().endTest(info);
+        } catch (RemoteException e) {
+            IgnoreRemoteFault(e);
+        }
+    }
+
+    /**
+     * A test started.
+     */
+    public void startTest(Test test) {
+        stats.incTestsStarted();
+        TestInfo info = new TestInfo(test);
+        try {
+            getListener().startTest(info);
+        } catch (RemoteException e) {
+            IgnoreRemoteFault(e);
+        }
+    }
+
+
+}
---
+++ tags/release3_04_012_beta/core/smartfrog/src/org/smartfrog/services/management/SFDeployDisplay.java
@@ -0,0 +1,384 @@
+/** (C) Copyright 1998-2004 Hewlett-Packard Development Company, LP
+
+This library is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public
+License as published by the Free Software Foundation; either
+version 2.1 of the License, or (at your option) any later version.
+
+This library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public
+License along with this library; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+For more information: www.smartfrog.org
+
+*/
+package org.smartfrog.services.management;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.net.InetAddress;
+import java.rmi.RemoteException;
+
+import javax.swing.JButton;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTree;
+import javax.swing.JMenu;
+import javax.swing.JCheckBoxMenuItem;
+
+import org.smartfrog.services.display.Display;
+import org.smartfrog.services.display.SFDisplay;
+import org.smartfrog.sfcore.common.Context;
+import org.smartfrog.sfcore.common.SmartFrogCoreKeys;
+import org.smartfrog.sfcore.common.SmartFrogDeploymentException;
+import org.smartfrog.sfcore.common.SmartFrogException;
+import org.smartfrog.sfcore.prim.Prim;
+import org.smartfrog.sfcore.prim.TerminationRecord;
+import org.smartfrog.sfcore.processcompound.SFProcess;
+import org.smartfrog.sfcore.reference.Reference;
+import org.smartfrog.services.display.WindowUtilities;
+import org.smartfrog.sfcore.common.Logger;
+
+
+/**
+ *  Management Console component. It can be deployed as a normat SmartFrog
+ *  component or it can be started as a separate console.
+ */
+public class SFDeployDisplay extends SFDisplay implements ActionListener {
+   protected JButton refresh = new JButton();
+   private JPanel panelTree = null;
+   private JTree tree = null;
+   private JScrollPane scrollPaneTree = null;
+
+
+   /**
+    * Constructs SFDeployDisplay object
+    *
+    *@throws  RemoteException  If network or RMI error
+    */
+   public SFDeployDisplay() throws RemoteException {
+      super();
+   }
+
+
+   /**
+    *  The main program for the SFDeployDisplay class
+    *
+    *@param  args           The command line arguments
+    *@throws  Exception  If any error
+    */
+   public static void main(String[] args) throws Exception {
+      Logger.log("Starting management window...");
+
+      String nameDisplay = "sfManagementConsole";
+      int height = 440;
+      int width = 600;
+      OptionSet opts = new OptionSet(args);
+
+      if (opts.errorString != null) {
+         exitWith(opts.errorString);
+      }
+
+      final boolean showRootProcess = opts.showRootProcess;
+      final String hostname = opts.host;
+      final int port = opts.port;
+      String positionDisplay = opts.windowPosition;
+
+      try {
+         startConsole(nameDisplay, height, width, positionDisplay,
+               showRootProcess, hostname, port, true);
+      } catch (java.net.UnknownHostException uex) {
+         exitWith("Error: Unknown host.");
+      } catch (java.rmi.ConnectException cex) {
+         exitWith("Error: " + cex.getMessage());
+      } catch (Exception e) {
+         e.printStackTrace();
+         exitWith("Error in SFDeployDisplay.main():" + e);
+      }
+
+      Logger.log("Running.");
+   }
+
+
+   /**
+    * Starts the console
+    *
+    *@param  nameDisplay       name of the display
+    *@param  height            height of the window
+    *@param  width             width of the window
+    *@param  positionDisplay   position  of display
+    *@param  showRootProcess   boolean to enable display of root process
+    *@param  hostname          host name
+    *@param  port              port
+    *@param  shouldSystemExit  boolean to indicate exit at close of window
+    *@return                   Display object
+    *@throws  Exception     In case of any error
+    *
+    */
+
+
+   public static Display startConsole(String nameDisplay, int height,
+         int width, String positionDisplay, final boolean showRootProcess,
+         final String hostname, final int port, boolean shouldSystemExit)
+          throws Exception {
+      final JButton refreshButton;
+      JMenu jMenuMng = new JMenu();
+      final JCheckBoxMenuItem jCheckBoxMenuItemShowRootProcessPanel = new JCheckBoxMenuItem();
+      String infoConnection = ("sfManagementConsole connecting to " +
+            hostname + ":" + port);
+      Logger.log(infoConnection);
+      nameDisplay = nameDisplay + " [" + "sfManagementConsole connected to " +
+            hostname + ":" + port + "]";
+
+      if (showRootProcess) {
+         Logger.log(" showing rootProcess");
+      } else {
+         //System.out.println("");
+      }
+
+      final Display newDisplay;
+
+      if (org.smartfrog.services.display.WindowUtilities.areGraphicsAvailable()) {
+         newDisplay = new Display(nameDisplay, null);
+         newDisplay.setShouldSystemExit(shouldSystemExit);
+         newDisplay.setVisible(false);
+         newDisplay.setSize(width, height);
+         org.smartfrog.services.display.WindowUtilities.setPositionDisplay(null,
+               newDisplay, positionDisplay);
+
+         // Button for Refresh view ...
+         refreshButton = new JButton();
+         refreshButton.setText("Refresh");
+         refreshButton.setActionCommand("refreshButton");
+         refreshButton.addActionListener(
+            new ActionListener() {
+               public void actionPerformed(ActionEvent e) {
+                  //System.out.println("ActionEvent SFDEployDisplay: "+ e);
+                  if ((e.getActionCommand()).equals("refreshButton")) {
+                     newDisplay.cleanAddedPanels();
+                     try {
+                        addProcessesPanels(newDisplay, jCheckBoxMenuItemShowRootProcessPanel.isSelected(), //showRootProcess,
+                              hostname, port);
+                     } catch (Exception ex) {
+                        Logger.log(ex);
+//                        System.err.println();
+//                        ex.printStackTrace();
+                        exitWith("Error in SFDeployDisplay.refresh():" + ex);
+                     }
+                  }
+               }
+            });
+         newDisplay.mainToolBar.add(refreshButton);
+         newDisplay.showToolbar(true);
+         // Add deployTreePanel menu items
+         jMenuMng.setText("Mng. Console");
+         newDisplay.jMenuBarDisplay.add(jMenuMng);
+         jCheckBoxMenuItemShowRootProcessPanel.setSelected(showRootProcess);
+         jCheckBoxMenuItemShowRootProcessPanel.setText("Show rootProcess");
+         jCheckBoxMenuItemShowRootProcessPanel.setAccelerator(javax.swing.KeyStroke.getKeyStroke(77, java.awt.event.KeyEvent.CTRL_MASK | java.awt.event.KeyEvent.ALT_MASK, false));
+         jCheckBoxMenuItemShowRootProcessPanel.addActionListener(new ActionListener() {
+               public void actionPerformed(ActionEvent e) {
+                            refreshButton.doClick();
+               }
+         });
+         jMenuMng.add(jCheckBoxMenuItemShowRootProcessPanel);
+
+         newDisplay.setVisible(true);
+         addProcessesPanels(newDisplay, showRootProcess, hostname, port);
+
+         return newDisplay;
+      }
+
+      return null;
+   }
+
+
+   /**
+    *  Prints given error string and exits system
+    *
+    *@param  str  string to print on out
+    */
+   public static void exitWith(String str) {
+      if (str != null) {
+         System.err.println(str);
+      }
+
+      System.exit(1);
+   }
+
+
+   // Used in Main
+   /**
+    *  Adds a feature to the ProcessesPanels attribute of the SFDeployDisplay
+    *  class
+    *
+    *@param  display              The feature to be added to the ProcessesPanels
+    *      attribute
+    *@param  addRootProcessPanel  The feature to be added to the ProcessesPanels
+    *      attribute
+    *@param  hostname             The feature to be added to the ProcessesPanels
+    *      attribute
+    *@param  port                 The feature to be added to the ProcessesPanels
+    *      attribute
+    *@throws  Exception        If any error
+    */
+   public static void addProcessesPanels(Display display,
+         boolean addRootProcessPanel, String hostname, int port)
+          throws Exception {
+      int indexPanel = 0;
+
+      // Adding pannels
+      JPanel deployPanel = null;
+
+      if (addRootProcessPanel) {
+         deployPanel = new DeployTreePanel(SFProcess.getRootLocator()
+               .getRootProcessCompound(InetAddress.getByName(
+               hostname), port), true);
+         deployPanel.setEnabled(true);
+         display.tabPane.add(deployPanel, "rootProcess...", indexPanel++);
+      }
+
+      Context context = SFProcess.getRootLocator()
+            .getRootProcessCompound(InetAddress.getByName(
+            hostname), port).sfContext();
+      java.util.Enumeration keys = context.keys();
+      Object key = "";
+      Object value = "";
+
+      while (keys.hasMoreElements()) {
+         key = keys.nextElement();
+
+         //out.println("* " + key + ": " + (context.get((String)key)).toString());
+         value = context.get((String) key);
+
+         //System.out.println("* " + key + ": " + value.toString());
+         if (value instanceof Prim) {
+            if (((Prim) value).sfParent() == null) {
+               deployPanel = new DeployTreePanel(value, false);
+               deployPanel.setEnabled(true);
+               display.tabPane.add(deployPanel, key, indexPanel++);
+            }
+         }
+      }
+      display.tabPane.setSelectedIndex(0);
+   }
+
+
+   /**
+    * Deploys display component.
+    *
+    *@throws  SmartFrogException  If unable to deploy the component
+    *@throws  RemoteException     If RMI or network error
+    */
+   public synchronized void sfDeploy() throws SmartFrogException, RemoteException {
+//      try {
+         super.sfDeploy();
+
+         // We add the new Tree component here
+         Object root = this.sfResolveWithParser(SmartFrogCoreKeys.SF_ROOT);
+
+         //root= new CompoundImpl();
+         //System.out.println("Root: "+root.toString());
+         this.panelTree = new DeployTreePanel(root, false);
+         this.panelTree.setEnabled(true);
+
+         display.tabPane.add(panelTree, "Deploy Deployed System ...", 0);
+
+         this.display.screen.append("\n Version: " +
+               org.smartfrog.Version.versionString + "\n");
+
+         // Button for Refresh view ...
+         refresh.setText("Refresh");
+         refresh.setActionCommand("refreshButton");
+         refresh.addActionListener(this);
+         display.mainToolBar.add(this.refresh);
+         display.showToolbar(true);
+      //end panelTree example
+   }
+
+
+   /**
+    * Starts the display component.
+    *
+    *@throws  SmartFrogException  If unable to start the component
+    *@throws  RemoteException     If RMI or network error
+    */
+   public synchronized void sfStart() throws SmartFrogException, RemoteException {
+      super.sfStart();
+   }
+
+
+   /**
+    * Terminates the SFDeployDisplay component
+    *
+    *@param  t  The reason why it was terminated
+    */
+   public synchronized void sfTerminateWith(TerminationRecord t) {
+      this.display.dispose();
+      super.sfTerminateWith(t);
+   }
+
+
+   /**
+    *  Main processing method for the SFDeployDisplay object
+    */
+   public void run() { }
+
+
+   /**
+    * Method of interface PrintMsgInt
+    *
+    *@param  msg  message to print
+    */
+   public void printMsg(String msg) {
+      super.printMsg(msg);
+
+      // We print in the output
+      ((DeployTreePanel) panelTree).add(msg);
+   }
+
+
+   /**
+    *  Refreshes the display panel.
+    */
+   public void refresh() {
+      try {
+         //System.out.println("Refreshing info");
+         Object root = this.sfResolve(SmartFrogCoreKeys.SF_ROOT,true);
+         ((DeployTreePanel) panelTree).setModel(root);
+         ((DeployTreePanel) panelTree).refresh();
+      } catch (Throwable ex) {
+         Logger.logQuietly("Failure refresh() SFDeployDisplay!",ex);
+      }
+   }
+
+
+   /**
+    * Interface Method.
+    *
+    *@param  e  action event
+    */
+   public void actionPerformed(ActionEvent e) {
+      Logger.log("ActionEvent SFDEployDisplay: "+ e);
+      if ((e.getActionCommand()).equals("refreshButton")) {
+         refresh(e);
+      }
+
+   }
+
+
+   /**
+    * Interface Method.
+    *
+    *@param  e  acrtion event
+    */
+   private void refresh(ActionEvent e) {
+      this.refresh();
+   }
+
+}
