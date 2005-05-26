a6302244194f938e30a007df682d20ce6df3ed61
nobody <nobody@9868f95a-be1e-0410-b3e3-a02e98b909e6>
2005-05-26 18:06:22 +0000

    This commit was manufactured by cvs2svn to create tag 'release3_04_031_beta'.

---
+++ tags/release3_04_031_beta/core/extras/cdl/src/org/smartfrog/sfcore/languages/cdl/dom/Import.java
@@ -0,0 +1,65 @@
+/** (C) Copyright 2005 Hewlett-Packard Development Company, LP
+
+ This library is free software; you can redistribute it and/or
+ modify it under the terms of the GNU Lesser General Public
+ License as published by the Free Software Foundation; either
+ version 2.1 of the License, or (at your option) any later version.
+
+ This library is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ Lesser General Public License for more details.
+
+ You should have received a copy of the GNU Lesser General Public
+ License along with this library; if not, write to the Free Software
+ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+ For more information: www.smartfrog.org
+
+ */
+package org.smartfrog.sfcore.languages.cdl.dom;
+
+import nu.xom.Element;
+import nu.xom.Node;
+import org.ggf.cddlm.generated.api.CddlmConstants;
+import org.smartfrog.sfcore.languages.cdl.CdlParsingException;
+
+/**
+ * Import element
+ * created 21-Apr-2005 14:26:27
+ */
+
+public class Import extends DocNode {
+
+    public String namespace;
+
+    public String location;
+
+    public Import() {
+    }
+
+    public Import(Element node) throws CdlParsingException {
+        fromXML(node);
+    }
+
+    /**
+     * Parse from XM
+     *
+     * @throws CdlParsingException
+     */
+    public void fromXML(Element element) throws CdlParsingException {
+        namespace = getCdlAttribute(element, ATTR_NAMESPACE, false);
+        location = getCdlAttribute(element, ATTR_LOCATION, true);
+    }
+
+
+    /**
+     * test that a node is of the right type
+     * @param element
+     * @return true if the element namespace and localname match what we handle
+     */
+    static boolean isA(Element element) {
+        return isNode(element,ELEMENT_IMPORT);
+    }
+
+}
