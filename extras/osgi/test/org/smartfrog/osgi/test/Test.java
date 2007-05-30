62995a7d45c190a2867ed112203ddbe79d77de39
aftereight <aftereight@9868f95a-be1e-0410-b3e3-a02e98b909e6>
2007-05-30 16:41:22 +0000

    preparing bundle deployer

--- extras/osgi/test/org/smartfrog/osgi/test/Test.java
+++
@@ -1,9 +0,0 @@
-package org.smartfrog.osgi.test;
-
-import org.smartfrog.sfcore.prim.Prim;
-
-import java.rmi.RemoteException;
-
-public interface Test extends Prim {
-    void sayHello() throws RemoteException;
-}
