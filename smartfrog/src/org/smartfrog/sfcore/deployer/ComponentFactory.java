d86edef489ee18a34b418af93dde94458d84e05b
aftereight <aftereight@9868f95a-be1e-0410-b3e3-a02e98b909e6>
2007-06-26 14:51:25 +0000

    build repaired. more work on component factories

--- smartfrog/src/org/smartfrog/sfcore/deployer/ComponentFactory.java
+++ smartfrog/src/org/smartfrog/sfcore/deployer/ComponentFactory.java
@@ -1,11 +1,13 @@
 package org.smartfrog.sfcore.deployer;
 
 import org.smartfrog.sfcore.common.SmartFrogDeploymentException;
+import org.smartfrog.sfcore.common.SmartFrogResolutionException;
 import org.smartfrog.sfcore.componentdescription.ComponentDescription;
 import org.smartfrog.sfcore.prim.Prim;
 
 import java.rmi.RemoteException;
 
 public interface ComponentFactory extends Prim {
-    Prim getComponent(ComponentDescription askedFor) throws RemoteException, SmartFrogDeploymentException;
+    Prim getComponent(ComponentDescription askedFor) throws RemoteException,
+            ClassNotFoundException, InstantiationException, IllegalAccessException, SmartFrogResolutionException;
 }
