d9897ed3d3685dd8d1fe9341b9ad3785bb3daf24
aftereight <aftereight@9868f95a-be1e-0410-b3e3-a02e98b909e6>
2007-07-26 15:16:20 +0000

    logging output from remote equinox console

--- extras/osgi/daemon-bundle/src/org/smartfrog/osgi/EquinoxSubprocessStarterImpl.java
+++ extras/osgi/daemon-bundle/src/org/smartfrog/osgi/EquinoxSubprocessStarterImpl.java
@@ -17,6 +17,8 @@ import org.smartfrog.sfcore.processcompound.ProcessCompound;
 import java.io.File;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.net.Socket;
 import java.net.URL;
 import java.util.ArrayList;
@@ -83,14 +85,28 @@ public class EquinoxSubprocessStarterImpl extends AbstractSubprocessStarter {
                 if (log.isDebugEnabled()) log.debug("Installing: " + toInstall[i].getLocation());
                 writer.println("install " + toInstall[i].getLocation());
                 if (isNotFragment(i)) {
-                    // 0 is the bundle ID of the system bundle, which is already there
+                    // 0 is the bundle ID of the system bundle, which is already there. So we need to bump IDs
                     writer.println("start " + (i + 1));
                 }
+                logConsoleOutput(socket);
             }
 
         } finally {
             if (writer != null) writer.close();
             if (socket != null) socket.close();
+        }
+    }
+
+    private void logConsoleOutput(Socket socket) throws IOException {
+        if (log.isDebugEnabled()) {
+            BufferedReader reader = null;
+            try {            
+                reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                String line;
+                while ((line = reader.readLine()) != null) log.debug(line);
+            } finally {
+                if (reader != null) reader.close();
+            }
         }
     }
 
