1576fc0838142c79058491ab17b836eb36ebce20
aftereight <aftereight@9868f95a-be1e-0410-b3e3-a02e98b909e6>
2007-08-10 10:43:42 +0000

    environment descriptions are now beautiful. thank julio!

--- branches/core-branch-osgi/smartfrog/src/org/smartfrog/sfcore/deployer/ApplicationEnvironmentImpl.java
+++ branches/core-branch-osgi/smartfrog/src/org/smartfrog/sfcore/deployer/ApplicationEnvironmentImpl.java
@@ -3,10 +3,12 @@ package org.smartfrog.sfcore.deployer;
 import org.smartfrog.sfcore.common.SmartFrogException;
 import org.smartfrog.sfcore.componentdescription.ComponentDescription;
 import org.smartfrog.sfcore.compound.CompoundImpl;
+import org.smartfrog.sfcore.prim.PrimImpl;
+import org.smartfrog.sfcore.processcompound.SFProcess;
 
 import java.rmi.RemoteException;
 
-public class ApplicationEnvironmentImpl extends CompoundImpl implements ApplicationEnvironment {
+public class ApplicationEnvironmentImpl extends PrimImpl implements ApplicationEnvironment {
     private static final String PARSER = "parser";
     private static final String TARGET = "target";
     
@@ -16,7 +18,7 @@ public class ApplicationEnvironmentImpl extends CompoundImpl implements Applicat
         ComponentDescription parserEnv = (ComponentDescription) sfResolve(PARSER);
         ComponentDescription targetEnv = (ComponentDescription) sfResolve(TARGET);
 
-        sfCreateNewApp(null, parserEnv, null);
-        sfCreateNewApp(null, targetEnv, null);
+        SFProcess.getProcessCompound().sfCreateNewApp(null, parserEnv, null);
+        SFProcess.getProcessCompound().sfCreateNewApp(null, targetEnv, null);
     }
 }
