d7d4ace5104419ed9be4b421a4bf5a061b44d2f1
anfarr <anfarr@9868f95a-be1e-0410-b3e3-a02e98b909e6>
2007-10-13 00:47:55 +0000

    Delete csf

--- branches/core-branch-constraints/smartfrog/src/org/smartfrog/sfcore/languages/csf/constraints/CDBrowserModel.java
+++
@@ -1,8 +0,0 @@
-package org.smartfrog.sfcore.languages.csf.constraints;
-
-public interface CDBrowserModel {
-	void setES(Object es);
-	Object attr(Object d, Object av);
-	void redraw();
-	void kill();
-}
