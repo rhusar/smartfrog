5df5d9eebe80f8ac05103c3f459186bd3a331f52
julgui <julgui@9868f95a-be1e-0410-b3e3-a02e98b909e6>
2008-08-07 14:33:09 +0000

    Merge with latest smartfrog core. Only core without workflow.

--- smartfrog/src/org/smartfrog/sfcore/workflow/conditional/conditions/AbstractCompoundCondition.java
+++ smartfrog/src/org/smartfrog/sfcore/workflow/conditional/conditions/AbstractCompoundCondition.java
@@ -25,11 +25,14 @@ import org.smartfrog.sfcore.common.SmartFrogException;
 import org.smartfrog.sfcore.common.SmartFrogResolutionException;
 import org.smartfrog.sfcore.common.SmartFrogDeploymentException;
 import org.smartfrog.sfcore.prim.Prim;
+import org.smartfrog.sfcore.prim.Liveness;
 
 import java.rmi.RemoteException;
 import java.util.Enumeration;
 
 /**
+ * This is an abstract base class for compound conditions; those that nest
+ * other conditions internally -and, or, xor, etc.
  * created 30-Nov-2006 14:13:51
  */
 
@@ -59,9 +62,8 @@ public abstract class AbstractCompoundCondition extends ConditionCompound {
     /**
     * Starts the component by deploying the condition
     *
-    * @throws org.smartfrog.sfcore.common.SmartFrogException
-    *                                  in case of problems creating the child
-    * @throws java.rmi.RemoteException In case of network/rmi error
+    * @throws SmartFrogException  in case of problems creating the child
+    * @throws RemoteException In case of network/rmi error
     */
     public synchronized void sfStart() throws SmartFrogException, RemoteException {
         super.sfStart();
@@ -72,31 +74,24 @@ public abstract class AbstractCompoundCondition extends ConditionCompound {
 
     /**
      * Check that everything is a condition
-     * @return
-     * @throws RemoteException
-     * @throws SmartFrogException
+     * @throws RemoteException for network problems
+     * @throws SmartFrogException for any other problem
      */
     public synchronized void verifyConditional() throws RemoteException, SmartFrogException {
-        Enumeration children = sfChildren();
-        while (children.hasMoreElements()) {
-            Object o = children.nextElement();
-            if (o instanceof Prim) {
-                //ignore things that are not components
-                Prim prim = (Prim) o;
-                if (!(prim instanceof Condition)) {
-                    //but require everything to be a condition
-                    throw new SmartFrogDeploymentException("Not a Condition", prim);
-                }
-            }
+    	for (Prim prim:sfChildList()) {
+    		if (!(prim instanceof Condition)) {
+    			//but require everything to be a condition
+    			throw new SmartFrogDeploymentException("Not a Condition", prim);
+    		}
         }
     }
 
 
     /**
      * Stop the parent from deploying a condition
-     * @throws SmartFrogResolutionException
-     * @throws RemoteException
-     * @throws SmartFrogDeploymentException
+     * @throws SmartFrogResolutionException not thrown
+     * @throws SmartFrogDeploymentException not thrown 
+     * @throws RemoteException for network problems
      */
     protected void deployCondition()
             throws SmartFrogResolutionException, RemoteException, SmartFrogDeploymentException {
@@ -107,12 +102,11 @@ public abstract class AbstractCompoundCondition extends ConditionCompound {
      * Evaluate the condition by delegating to the underlying condition.
      *
      * @return true if it is successful, false if not
-     * @throws java.rmi.RemoteException for network problems
-     * @throws org.smartfrog.sfcore.common.SmartFrogException
-     *                                  for any other problem
+     * @throws RemoteException for network problems
+     * @throws SmartFrogException for any other problem
      */
     public synchronized boolean evaluate() throws RemoteException, SmartFrogException {
-        Enumeration children = sfChildren();
+        Enumeration<Liveness> children = sfChildren();
         boolean keepGoing = true;
         resetAccumulator();
         while (keepGoing && children.hasMoreElements()) {
