51b2713e224f5ba4d3e27ba9bf2f6fba3e7476fb
steve_l <steve_l@9868f95a-be1e-0410-b3e3-a02e98b909e6>
2008-09-17 13:59:24 +0000

    release release3.12.036 (3.12.036) from revision 6457 on 2008-09-17 14:59 by slo

---
+++ tags/release3.12.036/core/components/csvfiles/src/org/smartfrog/services/filesystem/csvfiles/CSVColumnReader.java
@@ -0,0 +1,174 @@
+/* (C) Copyright 2008 Hewlett-Packard Development Company, LP
+
+This library is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public
+License as published by the Free Software Foundation; either
+version 2.1 of the License, or (at your option) any later version.
+
+This library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public
+License along with this library; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+For more information: www.smartfrog.org
+
+*/
+package org.smartfrog.services.filesystem.csvfiles;
+
+import org.smartfrog.sfcore.common.SmartFrogDeploymentException;
+import org.smartfrog.sfcore.common.SmartFrogException;
+import org.smartfrog.sfcore.prim.Prim;
+import org.smartfrog.services.filesystem.TupleReaderThread;
+import org.smartfrog.services.filesystem.TupleDataSource;
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+import java.util.Vector;
+
+/**
+ * component to read a column of a CSV into a single vector and attach it to a componennt
+ */
+
+public class CSVColumnReader extends AbstractCSVProcessor implements Remote {
+
+    /**
+     * Column number
+     */
+    public static final String ATTR_COLUMN = "column";
+    /**
+     * optional target prim to set the attribute on
+     */
+    public static final String ATTR_TARGET = "target";
+    /**
+     * name of the attribute to set
+     */
+    public static final String ATTR_TARGET_ATTRIBUTE = "targetAttribute";
+
+    /**
+     * skip empty "" entries?
+     */
+    public static final String ATTR_SKIP_EMPTY_FIELDS = "skipEmptyFields";
+
+    /**
+     * skip empty "" entries?
+     */
+    public static final String ATTR_SKIP_NARROW_LINES = "skipNarrowLines";
+    /**
+     * {@value}
+     */
+    private static final String ATTR_TRIM_FIELDS = "trimFields";
+    /**
+     * the attribute we set
+     */
+    public static final String ATTR_RESULT = "result";
+    private Prim target;
+    private boolean skipEmptyFields;
+    private String targetAttribute;
+    private int column;
+    private boolean skipNarrowLines;
+    private boolean trimFields;
+
+
+    public CSVColumnReader() throws RemoteException {
+    }
+
+    /**
+     * Can be called to start components. Subclasses should override to provide functionality Do not block in this call,
+     * but spawn off any main loops!
+     *
+     * @throws SmartFrogException failure while starting
+     * @throws RemoteException In case of network/rmi error
+     */
+    public synchronized void sfStart() throws SmartFrogException, RemoteException {
+        super.sfStart();
+        TupleDataSource source = (TupleDataSource) sfResolve(ATTR_SOURCE, (Prim) null, true);
+        target = sfResolve(ATTR_TARGET, (Prim) null, false);
+        targetAttribute = sfResolve(ATTR_TARGET_ATTRIBUTE, "", target != null);
+        trimFields = sfResolve(ATTR_TRIM_FIELDS, false, true);
+        skipEmptyFields = sfResolve(ATTR_SKIP_EMPTY_FIELDS, false, true);
+        skipNarrowLines = sfResolve(ATTR_SKIP_NARROW_LINES, false, true);
+        column = sfResolve(ATTR_COLUMN, 0, true);
+        setReader(new ReaderThread(source));
+        getReader().start();
+    }
+
+
+    /**
+     * do the work in a thread which triggers workflow events afterwards
+     */
+    private class ReaderThread extends TupleReaderThread {
+        private Vector<String> result;
+
+
+        /**
+         * Create a basic thread
+         *
+         * @param source the data source
+         */
+        private ReaderThread(TupleDataSource source) {
+            super(CSVColumnReader.this, source, true);
+        }
+
+        /**
+         * we've finished (successfully)
+         *
+         * @throws SmartFrogException SmartFrog problems
+         * @throws RemoteException network problems
+         */
+        @Override
+        protected void onFinished() throws SmartFrogException, RemoteException {
+            //end of lines set the results
+            sfReplaceAttribute(ATTR_RESULT, result);
+            if (target != null) {
+                target.sfReplaceAttribute(targetAttribute, result);
+            }
+        }
+
+        /**
+         * Process one line of the data source
+         *
+         * @param line line to process
+         * @throws SmartFrogException SmartFrog problems
+         * @throws RemoteException network problems
+         */
+        @Override
+        protected void processOneLine(String[] line) throws SmartFrogException, RemoteException {
+            int w = line.length;
+            if (w < column) {
+                //too narrow for this column
+                if (skipNarrowLines) {
+                    return;
+                } else {
+                    throw new SmartFrogDeploymentException("Too narrow, line #" + getCurrentLine() + ": "
+                            + CSVFileReadImpl.merge(line), CSVColumnReader.this);
+                }
+            }
+            String columnValue = line[column - 1];
+            if (trimFields) {
+                columnValue = columnValue.trim();
+            }
+            if (columnValue.length() == 0 && skipEmptyFields) {
+                return;
+            }
+            result.add(columnValue);
+        }
+
+        /**
+         * we've started. do any preparation
+         *
+         * @throws SmartFrogException SmartFrog problems
+         * @throws RemoteException network problems
+         */
+        @Override
+        protected void onStarted() throws SmartFrogException, RemoteException {
+            result = new Vector<String>();
+        }
+
+
+    }
+
+}
---
+++ tags/release3.12.036/core/smartfrog/src/org/smartfrog/sfcore/utils/WorkflowThread.java
@@ -0,0 +1,115 @@
+/* (C) Copyright 2008 Hewlett-Packard Development Company, LP
+
+This library is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public
+License as published by the Free Software Foundation; either
+version 2.1 of the License, or (at your option) any later version.
+
+This library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public
+License along with this library; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+For more information: www.smartfrog.org
+
+*/
+package org.smartfrog.sfcore.utils;
+
+import org.smartfrog.sfcore.prim.Prim;
+import org.smartfrog.sfcore.prim.TerminationRecord;
+import org.smartfrog.sfcore.reference.Reference;
+
+import java.rmi.RemoteException;
+
+/**
+ * The Workflow thread will terminate its owner when finishing.
+ *
+ */
+
+public class WorkflowThread extends SmartFrogThread {
+    private Prim owner;
+    private Reference ownerID;
+    private boolean workflowTermination;
+
+    /**
+     * termination message: {@value}
+     */
+    public static final String WORKER_THREAD_COMPLETED = "Worker thread completed";
+
+    /**
+     * Allocates a new <code>SmartFrogThread</code> object.
+     *
+     * @param owner owner obj
+     * @param notifyObject to notify afterwards. If null, "this" is used
+     * @param workflowTermination should workflow (optional) termination rules be used? If false, this thread
+     * always terminates the parent when finished
+     */
+    public WorkflowThread(Prim owner, boolean workflowTermination,Object notifyObject) {
+        super(notifyObject);
+        this.owner = owner;
+        this.workflowTermination=workflowTermination;
+        try {
+            ownerID = owner.sfCompleteName();
+        } catch (RemoteException e) {
+            ownerID = null;
+        }
+    }
+
+    /**
+     * Create a basic thread. Notification is bound to a local notification object.
+     *
+     * @param owner owner thread
+     * @param workflowTermination is workflow termination expected
+     */
+    public WorkflowThread(Prim owner, boolean workflowTermination) {
+        this(owner,workflowTermination, new Object());
+    }
+
+    /**
+     * Runs the {@link #execute()} method, catching any exception it throws and storing it away for safe keeping After
+     * the run, the notify object is notified, and we trigger a workflow termination
+     */
+    @Override
+    public void run() {
+        //do the work and catch the result
+        super.run();
+        //now analyse the result, create a term record and maybe terminate the owner
+        boolean isNormal = getThrown() == null;
+        TerminationRecord tr = new TerminationRecord(
+                isNormal ? TerminationRecord.NORMAL : TerminationRecord.ABNORMAL,
+                getTerminationMessage(),
+                ownerID,
+                getThrown());
+        aboutToTerminate(tr);
+        ComponentHelper helper = new ComponentHelper(owner);
+        if (workflowTermination && isNormal) {
+            //put up for workflow termination
+            helper.sfSelfDetachAndOrTerminate(tr);
+        } else {
+            //workflow termination is disabled, or something went wrong
+            //put up for termination
+            helper.targetForTermination(tr, false, false, false);
+        }
+    }
+
+    /**
+     * this is an override point. The TR is passed in for examination and
+     * editing. The base implementation does nothing.
+     * @param tr the termination record about to be passed up
+     */
+    protected void aboutToTerminate(TerminationRecord tr) {
+
+    }
+
+    /**
+     * Override point: the termination message
+     * @return {@link #WORKER_THREAD_COMPLETED}
+     */
+    protected String getTerminationMessage() {
+        return WORKER_THREAD_COMPLETED;
+    }
+}
