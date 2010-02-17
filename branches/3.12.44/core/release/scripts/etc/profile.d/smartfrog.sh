153f828be3e3f5dc4bd4c5b751e8e87c1097ef0a
steve_l <steve_l@9868f95a-be1e-0410-b3e3-a02e98b909e6>
2010-02-17 13:25:39 +0000

    SFOS-1435 csh/tcsh scripts build the path up wrong

--- branches/3.12.44/core/release/scripts/etc/profile.d/smartfrog.sh
+++ branches/3.12.44/core/release/scripts/etc/profile.d/smartfrog.sh
@@ -1,9 +1,13 @@
 # This file contains environment variables needed by the Smartfrog Daemon
 
+# This script is copied with property expansion at build time, which can be 
+# used to force in dynamic values. Because of this, use $ and braced {} strings
+# carefully
+
 export SFHOME="${rpm.install.dir}"
 
 # Add the directory of the SmartFrog executables to the path
-if echo ":$${PATH-}:" | grep ":$${SFHOME}:" > /dev/null 2>&1
+if echo ":$${PATH-}:" | grep ":$${SFHOME}" > /dev/null 2>&1
 then
 	:
 else
